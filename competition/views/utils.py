86abcbe142f3ba00c776dac89da8f8740ad605b6
Param Kapur <paramkapur2002@gmail.com>
2020-06-26 12:28:31 +0530

    free competitions

--- competition/views/utils.py
+++ competition/views/utils.py
@@ -16,11 +16,14 @@ def grant_access_to_competition(user: User, competition: Competition) -> bool:
 def has_access_to_competition(user: User, competition: Competition) -> bool:
 	if not user.is_authenticated:
 		return False
-	try:
-		CompetitionAccess.objects.get(user=user, competition=competition)
+	if competition.cost > 0:
+		try:
+			CompetitionAccess.objects.get(user=user, competition=competition)
+			return True
+		except CompetitionAccess.DoesNotExist:
+			return False
+	else:
 		return True
-	except CompetitionAccess.DoesNotExist:
-		return False
 
 
 def generate_competition_codes(competition: Competition, number_of_codes: int) -> list:
