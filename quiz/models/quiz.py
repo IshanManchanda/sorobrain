90268cd52c712d62346c7df2c0de06b54b53d28a
Param Kapur <paramkapur2002@gmail.com>
2020-11-03 10:07:49 +0530

    fixed bug where score is not calculated due to seconds attr not occuring

--- quiz/models/quiz.py
+++ quiz/models/quiz.py
@@ -285,7 +285,7 @@ class QuizSubmission(models.Model):
 		else:
 			remaining_time = self.quiz.total_time - self.attempt_time
 		self.score = ((correct_answers_number / (correct_answers_number + incorrect_answers_number) * 100) * (
-					((remaining_time.seconds / self.quiz.total_time.seconds) * 100) / 10))
+				((abs(remaining_time.total_seconds()) / abs(self.quiz.total_time.total_seconds())) * 100) / 10))
 		self.save()
 
 		return result
